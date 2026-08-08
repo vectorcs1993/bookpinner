75bad1a2f48ea788a152ce9cc63751b8edc4e5fc
vectorcs1993 <vectorcs@mail.ru>
2026-08-08 22:15:54 +0700

    v0.3.2

--- quasar.config.js
+++ quasar.config.js
@@ -44,7 +44,7 @@ export default defineConfig((/* ctx */) => {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: '/bookpiner/',
+      publicPath: '/bookpinner/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
